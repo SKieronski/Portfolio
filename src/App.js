ee044b9b5d165c2855dd2bd37a33f19042a1bceb
SKieronski <GitPW123!@#>
2022-04-09 16:31:00 -0500

    Added Bulma CSS library. Setup view for desktop sizes.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,80 @@
+import "bulma/css/bulma.min.css";
 import './App.css';
-import HeaderBar from './components/HeaderBar';
+// import HeaderBar from './components/HeaderBar';
 import ProjectOutline from './components/ProjectOutline';
 
 function App() {
 
-
   return (
-    <div className="App">
+  <div className="App">
+    <section class="section is-large" id="ProjectSection">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <div class="block" />
+              <p class="title is-1">Wind</p>
+              <div class="content">
+                <p class="">
+                  Bored of running the same route over and over again? 
+                  Wind is a mobile app designed to provide new running routes for avid runners. 
+                  Using a distance (meters), bearing, and your location, Wind will calculate a running route for the user. 
+                  The app also features user authorization and route-saving and deleting functionality. 
+                </p>
+              </div>
+            <div class="block" />
+            <p class="title is-3">Tech Used</p>
+            {/* <table class="table">
+              <tbody>
+                <tr>
+                  <td>MongoDB</td>
+                  <td>Express</td>
+                  <td>React-Native</td>
+                  <td>Node.js</td>
+                  <td>Axios</td>
+                  <td>Expo</td>
+                  <td>Xcode</td>
+                  <td>Bcrypt</td>
+                  <td>JWT</td>
+                  <td>React-Native-Maps</td>
+                  <td>React-Navigation</td>
+                </tr>
+              </tbody>
+            </table> */}
+            <div class="content is-normal">
+              <ul className="TechListUL">
+                <li className="TechListLI"> MongoDB </li>
+                <li className="TechListLI"> Express </li>
+                <li className="TechListLI"> React-Native </li>
+                <li className="TechListLI"> Node.js </li>
+                <li className="TechListLI"> Axios </li>
+                <li className="TechListLI"> Expo </li>
+                <li className="TechListLI"> Xcode </li>
+                <li className="TechListLI"> Bcrypt </li>
+                <li className="TechListLI"> React-Native-Maps </li>
+                <li className="TechListLI"> React-Navigation </li>
+                <li className="TechListLI"> JWT </li>
+              </ul>
+            </div>
+            <a class="button is-link is-responsive" id="GitHubButton">
+              <span class="icon">
+                <i class="fab fa-github"/>
+              </span>
+              <span>GitHub Repo</span>
+            </a>
+
+          </div>
+          <div class="column is-narrow" id="IphoneImageContainer">
+            <figure class="image is-1by2">
+              <img alt="Wind Create Screen" src="images/Wind/CreateScreen.png" />
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
       {/* <HeaderBar /> */}
-      <ProjectOutline />
-    </div>
+      {/* <ProjectOutline />
+      <ProjectOutline /> */}
+  </div>
   );
 }
 
--- src/components/ProjectOutline.js
+++ src/components/ProjectOutline.js
@@ -1,10 +1,13 @@
 import React from "react"
-
+import './ProjectOutline.css'
 function ProjectOutline() {
     return(
-        <>
-            <p>This is my Project</p>
-        </>
+        <div className="ProjectBox">
+            <p id="Description">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            <img alt="Wind Create Screen" className="Image" src="images/Wind/CreateScreen.png" />
+            <img alt = "Wind Routes List" className="Image" id="ImageTwo" src="images/Wind/myroutes.png" />
+            
+        </div>
     )
 }
 
