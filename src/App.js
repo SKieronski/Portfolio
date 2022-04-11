bf4ef6cd65a940af9e9822c3e9396e31852d7f6b
SKieronski <GitPW123!@#>
2022-04-11 14:03:14 -0500

    Converted App.js code into a reusable component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import ProjectOutline from './components/ProjectOutline';
 
 const myWindTech = ["MongoDB", "Express", "React-Native", "Node.js", "Axios", "Expo"
   , "Xcode", "Bcrypt", "React-Native-Maps", "React-Navigation", "JWT"]
-
+const myWindTitle = "Wind"
+const myWindDescription = "Bored of running the same route over and over again? Wind is a mobile app designed to provide new running routes for avid runners. Using a distance (meters), bearing, and your location, Wind will calculate a running route for the user. The app also features user authorization and route-saving and deleting functionality."
+const myWindGithub = "https://github.com/SKieronski/Wind";
 function App() {
 
   return (
   <div className="App">
-    <section class="section is-large" id="ProjectSection">
+    <ProjectOutline techList={myWindTech} title={myWindTitle} description={myWindDescription} githubHref={myWindGithub}/>
+    {/* <section class="section is-medium" id="ProjectSection">
       <div class="container">
         <div class="columns">
           <div class="column" id="CenterText">
@@ -53,6 +56,49 @@ function App() {
         </div>
       </div>
     </section>
+    <section class="section is-medium" id="ProjectSection">
+      <div class="container">
+        <div class="columns">
+          <div class="column" id="CenterText">
+            <div class="block" />
+            <p class="title is-1" id="WindTitle">Wind</p>
+            <div class="content">
+              <p id="TitleSubText">
+                Bored of running the same route over and over again? 
+                Wind is a mobile app designed to provide new running routes for avid runners. 
+                Using a distance (meters), bearing, and your location, Wind will calculate a running route for the user. 
+                The app also features user authorization and route-saving and deleting functionality. 
+              </p>
+            </div>
+            <div class="block" />
+            <p class="title is-3" id="Tech">Tech Used</p>
+            <div class="columns is-mobile is-multiline is-centered">
+              {myWindTech.map((tech) => {
+                return (
+                  <div class="column is-narrow">
+                    <code class="html">
+                      {tech}
+                    </code>
+                  </div>
+                )
+              })}
+            </div>
+            <a class="button is-link is-responsive" id="GitHubButton" href="https://github.com/SKieronski/Wind">
+              <span class="icon">
+                <i class="fab fa-github"/>
+              </span>
+              <span>GitHub Repo</span>
+            </a>
+
+          </div>
+          <div class="column is-narrow is-one-fifth-fullhd is-one-third-desktop is-two-thirds-mobile" id="IphoneImageContainer">
+            <figure class="image is-1by2">
+              <img alt="Wind Create Screen" src="images/Wind/CreateScreen.png" />
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section> */}
   </div>
   );
 }
--- src/components/ProjectOutline.js
+++ src/components/ProjectOutline.js
@@ -1,13 +1,50 @@
+import "bulma/css/bulma.min.css";
 import React from "react"
 import './ProjectOutline.css'
-function ProjectOutline() {
+
+function ProjectOutline({techList, title, description, githubHref}) {
     return(
-        <div className="ProjectBox">
-            <p id="Description">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            <img alt="Wind Create Screen" className="Image" src="images/Wind/CreateScreen.png" />
-            <img alt = "Wind Routes List" className="Image" id="ImageTwo" src="images/Wind/myroutes.png" />
-            
-        </div>
+        <>
+        <section class="section is-medium" id="ProjectSection">
+        <div class="container">
+            <div class="columns">
+                <div class="column" id="CenterText">
+                    <div class="block" />
+                    <p class="title is-1" id="Title">{title}</p>
+                    <div class="content">
+                      <p id="TitleSubText">
+                          {description}
+                      </p>
+                    </div>
+                    <div class="block" />
+                    <p class="title is-3" id="Tech">Tech Used</p>
+                    <div class="columns is-mobile is-multiline is-centered">
+                        {techList.map((tech) => {
+                            return (
+                                <div class="column is-narrow">
+                                <code class="html">
+                                  {tech}
+                                </code>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <a class="button is-link is-responsive" id="GitHubButton" href={githubHref}>
+                        <span class="icon">
+                            <i class="fab fa-github"/>
+                        </span>
+                        <span>GitHub Repo</span>
+                    </a>
+                </div>
+                <div class="column is-narrow is-one-fifth-fullhd is-one-third-desktop is-two-thirds-mobile" id="IphoneImageContainer">
+                    <figure class="image is-1by2">
+                        <img alt="Wind Create Screen" src="images/Wind/CreateScreen.png" />
+                    </figure>
+                </div>
+                </div>
+            </div>
+        </section>
+        </>
     )
 }
 
