f50669553028136af75ac3f71ac14616a626236d
SKieronski <GitPW123!@#>
2022-04-13 12:52:26 -0500

    Changed domain names again.

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -7,19 +7,19 @@ function HeaderBar() {
         <>
             <nav class="navbar is-fixed-top is-light has-shadow is-spaced is-hidden-mobile" role="navigation" aria-label="main navigation">
                 <div class="navbar-brand">
-                    <a class="navbar-item" href="https://scottkieronski.netlify.app/#Hero">
+                    <a class="navbar-item" href="https://scottkieronski.com/#Hero">
                         <span id="name">Scott Kieronski</span>
                     </a>
                 </div>
                 <div class="navbar-menu is-active" id="navbarOptions">
                     <div class="navbar-end">
-                        <a class="navbar-item" href="https://scottkieronski.netlify.app/#ProjectSection">
+                        <a class="navbar-item" href="https://scottkieronski.com/#ProjectSection">
                             Projects
                         </a>
-                        <a class="navbar-item" href="https://scottkieronski.netlify.app/#About">
+                        <a class="navbar-item" href="https://scottkieronski.com/#About">
                             About
                         </a>
-                        <a class="navbar-item" href="https://scottkieronski.netlify.app/#Contact">
+                        <a class="navbar-item" href="https://scottkieronski.com/#Contact">
                             Contact
                         </a>
                     </div>
