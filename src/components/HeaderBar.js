e8759fe214b006b98e1ec6c55fd7a7fa376160d5
SKieronski <GitPW123!@#>
2022-04-13 12:27:52 -0500

    Fixed a href warning that caused Netlify build to not compile.

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -5,18 +5,13 @@ import "./HeaderBar.css"
 function HeaderBar() {
     return(
         <>
-            <nav class="navbar is-fixed-top is-light has-shadow is-spaced" role="navigation" aria-label="main navigation">
+            <nav class="navbar is-fixed-top is-light has-shadow is-spaced is-hidden-mobile" role="navigation" aria-label="main navigation">
                 <div class="navbar-brand">
                     <a class="navbar-item" href="http://localhost:3000/#Hero">
                         <span id="name">Scott Kieronski</span>
                     </a>
-                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarOptions">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
                 </div>
-                <div class="navbar-menu" id="navbarOptions">
+                <div class="navbar-menu is-active" id="navbarOptions">
                     <div class="navbar-end">
                         <a class="navbar-item" href="http://localhost:3000/#ProjectSection">
                             Projects
